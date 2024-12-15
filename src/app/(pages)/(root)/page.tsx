7642ff35ed73d2a4f752179df8b97f5c40856c28
okamoto haruhito <okamotoharuhito@okamotonoAir.lan>
2024-12-15 18:00:05 +0900

    データフェッチロジックの追加

--- src/app/(pages)/(root)/page.tsx
+++ src/app/(pages)/(root)/page.tsx
@@ -1,6 +1,8 @@
+import { fetchRepoData } from "@/utils/api";
 import Hero from "./_components/hero";
 
-export default function Home() {
+export default async function Home() {
+  const res =  await fetchRepoData();
   return (
     <main className="space-y-32 pt-16">
 	    {/* <p>ヒーローセクション</p> */}
